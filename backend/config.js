27d397083989bd9d5e68b5120b6a77d757de0320
Sine Theta Systems <mr.mayurchawla@gmail.com>
2024-04-04 16:32:24 +0530

    removing my key

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const MongoDBURL = "mongodb+srv://root:root@books-store-mern.qhcr0pk.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN"
+export const MongoDBURL = "put your mongodb url here"
